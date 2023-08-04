dffc2c69b8d092d25b78c5f82892406e3f984c1b
Justin <justin06.rosales@gmail.com>
2023-08-03 21:10:22 -0400

    fixed sound delay issue

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+addEventListener('resize', () => {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+
+    cancelAnimationFrame(animationId)
+    scoreContainer.style.display = 'none'
+    startGameContainer.style.display = 'flex'
+})
+
 const scoreContainer = document.querySelector('#scoreContainer')
 const scoreEl = document.querySelector('#scoreEl')
 const endScoreEl = document.querySelector('#endScoreEl')
@@ -14,25 +23,6 @@ const startGameContainer = document.querySelector('#startGameContainer')
 // variables
 const GAME_SPEED = 60
 const GRAVITY = 1
-const dieSoundEffect = new Audio('Media/die-sound-effect.mp3')
-const scoreSoundEffect = new Audio('Media/point-sound-effect.mp3')
-const flapSoundEffect = new Audio('Media/flap-sound-effect.mp3')
-const hitSoundEffect = new Audio ('Media/hit-sound-effect.mp3')
-const swooshSoundEffect = new Audio ('Media/swoosh-sound-effect.mp3')
-dieSoundEffect.volume = 0.1
-scoreSoundEffect.volume = 0.2
-flapSoundEffect.volume = 0.2
-hitSoundEffect.volume = 0.2
-swooshSoundEffect.volume = 0.2
-
-addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
-
-    cancelAnimationFrame(animationId)
-    scoreContainer.style.display = 'none'
-    startGameContainer.style.display = 'flex'
-})
 
 // utility functions
 function randomIntFromRange(min, max) {
@@ -132,13 +122,21 @@ function init() {
 function gameOver() {
     cancelAnimationFrame(animationId)
     clearInterval(timer)
-    player.isAlive = false
+    const hitSoundEffect = new Audio ('Media/hit-sound-effect.mp3')
+    hitSoundEffect.volume = 0.2
     hitSoundEffect.play()
     setTimeout(() => {
         scoreContainer.style.display = 'none'
         endScoreEl.innerHTML = score
         startGameContainer.style.display = 'flex'
+        const swooshSoundEffect = new Audio ('Media/swoosh-sound-effect.mp3')
+        swooshSoundEffect.volume = 0.2
         swooshSoundEffect.play()
+        if (player.y + player.radius < canvas.height) {
+            const dieSoundEffect = new Audio('Media/die-sound-effect.mp3')
+            dieSoundEffect.volume = 0.1
+            dieSoundEffect.play()
+        }
     }, 500)
 }
 
@@ -194,6 +192,8 @@ function animate(currentTime) {
             pipe.isPassed = true
             
             // play score sound effect
+            const scoreSoundEffect = new Audio('Media/point-sound-effect.mp3')
+            scoreSoundEffect.volume = 0.2
             scoreSoundEffect.play()
         }
 
@@ -227,6 +227,8 @@ function animate(currentTime) {
 addEventListener('click', () => {
     if (player.isAlive) {
         player.velocity.y = player.flapStrength
+        const flapSoundEffect = new Audio('Media/flap-sound-effect.mp3')
+        flapSoundEffect.volume = 0.2
         flapSoundEffect.play()
     }
 })
@@ -235,5 +237,7 @@ startGameBtn.addEventListener('click', () => {
     init()
     animate()
     spawnPipes()
+    const swooshSoundEffect = new Audio ('Media/swoosh-sound-effect.mp3')
+    swooshSoundEffect.volume = 0.2
     swooshSoundEffect.play()
 })
