c90bdc83ed5a3472cb7477872b1b2441be3e30bb
Justin <justin06.rosales@gmail.com>
2024-06-03 12:30:19 -0400

    modify media folder name

--- script.js
+++ script.js
@@ -1,243 +1,278 @@
 // canvas setup
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
+addEventListener("resize", () => {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
 
-    cancelAnimationFrame(animationId)
-    scoreContainer.style.display = 'none'
-    startGameContainer.style.display = 'flex'
-})
+	cancelAnimationFrame(animationId);
+	scoreContainer.style.display = "none";
+	startGameContainer.style.display = "flex";
+});
 
-const scoreContainer = document.querySelector('#scoreContainer')
-const scoreEl = document.querySelector('#scoreEl')
-const endScoreEl = document.querySelector('#endScoreEl')
-const startGameBtn = document.querySelector('#startGameBtn')
-const startGameContainer = document.querySelector('#startGameContainer')
+const scoreContainer = document.querySelector("#scoreContainer");
+const scoreEl = document.querySelector("#scoreEl");
+const endScoreEl = document.querySelector("#endScoreEl");
+const startGameBtn = document.querySelector("#startGameBtn");
+const startGameContainer = document.querySelector("#startGameContainer");
 
 // variables
-const GAME_SPEED = 60
-const GRAVITY = 1
+const GAME_SPEED = 60;
+const GRAVITY = 1;
 
 // utility functions
 function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // objects
 class Player {
-    constructor(x, y, radius, color, velocity, flapStrength, isAlive) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-        this.velocity = velocity
-        this.flapStrength = flapStrength
-        this.isAlive = isAlive
-    }
+	constructor(x, y, radius, color, velocity, flapStrength, isAlive) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.velocity = velocity;
+		this.flapStrength = flapStrength;
+		this.isAlive = isAlive;
+	}
 
-    draw() {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        ctx.strokeStyle = 'black'
-        ctx.lineWidth = 1
-        ctx.stroke()
-        ctx.fillStyle = this.color
-        ctx.fill()
-    }
+	draw() {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 1;
+		ctx.stroke();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
 
-    update() {
-        this.draw()
+	update() {
+		this.draw();
 
-        // update velocity of player
-        this.velocity.y += GRAVITY
+		// update velocity of player
+		this.velocity.y += GRAVITY;
 
-        // update position
-        this.y += this.velocity.y
-    }
+		// update position
+		this.y += this.velocity.y;
+	}
 }
 class Pipe {
-    constructor(x, y, width, height, color, velocity, isPassed) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.color = color
-        this.velocity = velocity
-        this.isPassed = isPassed
-    }
+	constructor(x, y, width, height, color, velocity, isPassed) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+		this.velocity = velocity;
+		this.isPassed = isPassed;
+	}
 
-    draw() {
-        ctx.beginPath()
-        ctx.rect(this.x, this.y, this.width, this.height)
-        ctx.lineWidth = 4
-        ctx.stroke()
-        ctx.fillStyle = this.color
-        ctx.fill()
-    }
+	draw() {
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.lineWidth = 4;
+		ctx.stroke();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
 
-    update() {
-        this.draw()
+	update() {
+		this.draw();
 
-        // update position
-        this.x += this.velocity.x
-    }
+		// update position
+		this.x += this.velocity.x;
+	}
 }
 
 // implementation
-let player
-let pipes
-let score
-let timer
-let pipeSpawnSpeed
-let pipeGap
-let pointOnRect
-let bgColor
-let lastRenderTime
+let player;
+let pipes;
+let score;
+let timer;
+let pipeSpawnSpeed;
+let pipeGap;
+let pointOnRect;
+let bgColor;
+let lastRenderTime;
 
 function init() {
-    player = new Player(canvas.width / 2, canvas.height / 2, 30, '#ffdd1f', {y: 0}, -15, true)
-    pipes = []
-    score = 0
-    pipeSpawnSpeed = 2000
-    pipeGap = 205
-    pointOnRect = {
-        x: null,
-        y: null,
-    }
-    bgColor = 255
-    lastRenderTime = 0
-    scoreEl.innerHTML = score
-    scoreContainer.style.display = 'block'
-    endScoreEl.innerHTML = score
-    startGameContainer.style.display = 'none'
-    clearInterval(timer)
+	player = new Player(
+		canvas.width / 2,
+		canvas.height / 2,
+		30,
+		"#ffdd1f",
+		{ y: 0 },
+		-15,
+		true
+	);
+	pipes = [];
+	score = 0;
+	pipeSpawnSpeed = 2000;
+	pipeGap = 205;
+	pointOnRect = {
+		x: null,
+		y: null,
+	};
+	bgColor = 255;
+	lastRenderTime = 0;
+	scoreEl.innerHTML = score;
+	scoreContainer.style.display = "block";
+	endScoreEl.innerHTML = score;
+	startGameContainer.style.display = "none";
+	clearInterval(timer);
 }
 
 function gameOver() {
-    cancelAnimationFrame(animationId)
-    clearInterval(timer)
-    const hitSoundEffect = new Audio ('Media/hit-sound-effect.mp3')
-    hitSoundEffect.volume = 0.2
-    hitSoundEffect.play()
-    setTimeout(() => {
-        scoreContainer.style.display = 'none'
-        endScoreEl.innerHTML = score
-        startGameContainer.style.display = 'flex'
-        const swooshSoundEffect = new Audio ('Media/swoosh-sound-effect.mp3')
-        swooshSoundEffect.volume = 0.2
-        swooshSoundEffect.play()
-        if (player.y + player.radius < canvas.height) {
-            const dieSoundEffect = new Audio('Media/die-sound-effect.mp3')
-            dieSoundEffect.volume = 0.1
-            dieSoundEffect.play()
-        }
-    }, 500)
+	cancelAnimationFrame(animationId);
+	clearInterval(timer);
+	const hitSoundEffect = new Audio("media/hit-sound-effect.mp3");
+	hitSoundEffect.volume = 0.2;
+	hitSoundEffect.play();
+	setTimeout(() => {
+		scoreContainer.style.display = "none";
+		endScoreEl.innerHTML = score;
+		startGameContainer.style.display = "flex";
+		const swooshSoundEffect = new Audio("media/swoosh-sound-effect.mp3");
+		swooshSoundEffect.volume = 0.2;
+		swooshSoundEffect.play();
+		if (player.y + player.radius < canvas.height) {
+			const dieSoundEffect = new Audio("media/die-sound-effect.mp3");
+			dieSoundEffect.volume = 0.1;
+			dieSoundEffect.play();
+		}
+	}, 500);
 }
 
 function spawnPipes() {
-    timer = setInterval(() => {
-        const topPipe = {
-            width: 90,
-            height: canvas.height - pipeGap,
-            x: canvas.width,
-            y: -(canvas.height - pipeGap) * Math.random(),
-            color: '#18770d',
-            velocity: {x: -4.5}
-        }
-        const bottomPipe = {
-            width: topPipe.width,
-            height: topPipe.height,
-            x: topPipe.x,
-            y: topPipe.y + topPipe.height + pipeGap,
-            color: topPipe.color,
-            velocity: topPipe.velocity
-        }
+	timer = setInterval(() => {
+		const topPipe = {
+			width: 90,
+			height: canvas.height - pipeGap,
+			x: canvas.width,
+			y: -(canvas.height - pipeGap) * Math.random(),
+			color: "#18770d",
+			velocity: { x: -4.5 },
+		};
+		const bottomPipe = {
+			width: topPipe.width,
+			height: topPipe.height,
+			x: topPipe.x,
+			y: topPipe.y + topPipe.height + pipeGap,
+			color: topPipe.color,
+			velocity: topPipe.velocity,
+		};
 
-        pipes.push(new Pipe(topPipe.x, topPipe.y, topPipe.width, topPipe.height, topPipe.color, topPipe.velocity, false))
-        pipes.push(new Pipe(bottomPipe.x, bottomPipe.y, bottomPipe.width, bottomPipe.height, bottomPipe.color, bottomPipe.velocity, false))
-    }, pipeSpawnSpeed)
+		pipes.push(
+			new Pipe(
+				topPipe.x,
+				topPipe.y,
+				topPipe.width,
+				topPipe.height,
+				topPipe.color,
+				topPipe.velocity,
+				false
+			)
+		);
+		pipes.push(
+			new Pipe(
+				bottomPipe.x,
+				bottomPipe.y,
+				bottomPipe.width,
+				bottomPipe.height,
+				bottomPipe.color,
+				bottomPipe.velocity,
+				false
+			)
+		);
+	}, pipeSpawnSpeed);
 }
 
 function clamp(min, max, value) {
-    if (value < min) return min
-    else if (value > max) return max
-    else return value
+	if (value < min) return min;
+	else if (value > max) return max;
+	else return value;
 }
 
 // animation loop
-let animationId
+let animationId;
 function animate(currentTime) {
-    animationId = requestAnimationFrame(animate)
+	animationId = requestAnimationFrame(animate);
 
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / GAME_SPEED) return
-    lastRenderTime = currentTime
+	const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+	if (secondsSinceLastRender < 1 / GAME_SPEED) return;
+	lastRenderTime = currentTime;
 
-    ctx.clearRect(0, 0, innerWidth, innerHeight)
+	ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-    player.update()
-    pipes.forEach((pipe, index) => {
-        pipe.update()
+	player.update();
+	pipes.forEach((pipe, index) => {
+		pipe.update();
 
-        // increment score
-        if (!pipe.isPassed && player.x > pipe.x +pipe.width) {
-            score += 0.5
-            scoreEl.innerHTML = score
-            pipe.isPassed = true
-            
-            // play score sound effect
-            const scoreSoundEffect = new Audio('Media/point-sound-effect.mp3')
-            scoreSoundEffect.volume = 0.2
-            scoreSoundEffect.play()
-        }
+		// increment score
+		if (!pipe.isPassed && player.x > pipe.x + pipe.width) {
+			score += 0.5;
+			scoreEl.innerHTML = score;
+			pipe.isPassed = true;
 
-        // player pipe collision
-        pointOnRect.x = clamp(pipe.x, pipe.x + pipe.width, player.x)
-        pointOnRect.y = clamp(pipe.y, pipe.y + pipe.height, player.y)
-        if (Math.hypot(player.x - pointOnRect.x, player.y - pointOnRect.y) - player.radius <= 0) {
-            gameOver()
-            }
+			// play score sound effect
+			const scoreSoundEffect = new Audio("media/point-sound-effect.mp3");
+			scoreSoundEffect.volume = 0.2;
+			scoreSoundEffect.play();
+		}
 
-        // prevent player from going above pipes
-        if (player.y - player.radius <= 0 && player.x + player.radius >= pipe.x) {
-            gameOver()
-        }
+		// player pipe collision
+		pointOnRect.x = clamp(pipe.x, pipe.x + pipe.width, player.x);
+		pointOnRect.y = clamp(pipe.y, pipe.y + pipe.height, player.y);
+		if (
+			Math.hypot(player.x - pointOnRect.x, player.y - pointOnRect.y) -
+				player.radius <=
+			0
+		) {
+			gameOver();
+		}
 
-        // remove pipe when off screen
-        if (pipe.x + pipe.width < 0) {
-            setTimeout(() => {
-                pipes.shift()
-            }, 0)
-        }
-    })
+		// prevent player from going above pipes
+		if (
+			player.y - player.radius <= 0 &&
+			player.x + player.radius >= pipe.x
+		) {
+			gameOver();
+		}
 
-    // player ground collision
-    if (player.y + player.radius >= canvas.height) {
-        gameOver()
-    }
+		// remove pipe when off screen
+		if (pipe.x + pipe.width < 0) {
+			setTimeout(() => {
+				pipes.shift();
+			}, 0);
+		}
+	});
+
+	// player ground collision
+	if (player.y + player.radius >= canvas.height) {
+		gameOver();
+	}
 }
 
 // flapping logic
-addEventListener('click', () => {
-    if (player.isAlive) {
-        player.velocity.y = player.flapStrength
-        const flapSoundEffect = new Audio('Media/flap-sound-effect.mp3')
-        flapSoundEffect.volume = 0.2
-        flapSoundEffect.play()
-    }
-})
+addEventListener("click", () => {
+	if (player.isAlive) {
+		player.velocity.y = player.flapStrength;
+		const flapSoundEffect = new Audio("media/flap-sound-effect.mp3");
+		flapSoundEffect.volume = 0.2;
+		flapSoundEffect.play();
+	}
+});
 
-startGameBtn.addEventListener('click', () => {
-    init()
-    animate()
-    spawnPipes()
-    const swooshSoundEffect = new Audio ('Media/swoosh-sound-effect.mp3')
-    swooshSoundEffect.volume = 0.2
-    swooshSoundEffect.play()
-})
+startGameBtn.addEventListener("click", () => {
+	init();
+	animate();
+	spawnPipes();
+	const swooshSoundEffect = new Audio("media/swoosh-sound-effect.mp3");
+	swooshSoundEffect.volume = 0.2;
+	swooshSoundEffect.play();
+});
